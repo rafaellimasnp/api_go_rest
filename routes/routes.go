02e369d66f3e576b7f22adc02c12c146734f0fde
rafaellimasnp <rafaelrock722@gmail.com>
2023-02-08 00:05:38 -0400

    Metodo Get

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,7 @@ import (
 func HandleResquest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
